d525d0ad8066f4795aa8da89c5f5f15fdbfe0034
Milan Pála <milan.pala@peckadesign.cz>
2016-11-28 09:17:48 +0100

    Přidán command pro naplnění fronty alive checků

---
+++ app/Check/Commands/PublishAliveChecksCommand.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Pd\Monitoring\Check\Commands;
+
+class PublishAliveChecksCommand extends \Symfony\Component\Console\Command\Command
+{
+
+	use \Pd\Monitoring\Commands\TNamedCommand;
+
+	/**
+	 * @var \Kdyby\RabbitMq\IProducer
+	 */
+	private $producer;
+
+	/**
+	 * @var \Pd\Monitoring\Check\ChecksRepository
+	 */
+	private $checksRepository;
+
+
+	public function __construct(
+		\Kdyby\RabbitMq\IProducer $producer,
+		\Pd\Monitoring\Check\ChecksRepository $checksRepository
+	) {
+		parent::__construct();
+
+		$this->producer = $producer;
+		$this->checksRepository = $checksRepository;
+	}
+
+
+	protected function configure()
+	{
+		parent::configure();
+
+		$this->setName($this->generateName());
+	}
+
+
+	protected function execute(
+		\Symfony\Component\Console\Input\InputInterface $input,
+		\Symfony\Component\Console\Output\OutputInterface $output
+	) {
+		$conditions = [
+			'type' => \Pd\Monitoring\Check\ICheck::TYPE_ALIVE,
+		];
+		$checks = $this->checksRepository->findAll($conditions);
+
+		foreach ($checks as $check) {
+			$this->producer->publish($check->id);
+		}
+
+		return 0;
+	}
+
+}
---
+++ app/Commands/TNamedCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Pd\Monitoring\Commands;
+
+use Nette;
+
+
+trait TNamedCommand
+{
+
+	private function generateName()
+	{
+		$className = __CLASS__;
+		$name = substr($className, 0);
+		$name = str_replace('Module', '', $name);
+		$name = str_replace('Commands', '', $name);
+		$name = str_replace('Command', '', $name);
+		$name = str_replace('\\', ':', $name);
+		$name = str_replace('::', ':', $name);
+		$name = Nette\Utils\Strings::replace($name, '~([^:])([A-Z])~', '$1-$2');
+		$name = strtolower($name);
+		$name = ltrim($name, '-');
+
+		return $name;
+	}
+}
