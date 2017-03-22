624d77e86d17ca63bcc74b195535505992fb27fa
Viktor Beránek <viktor.beranek@peckadesign.cz>
2017-03-22 11:13:03 +0100

    #35 Zapauzování alertování podle času - Projektu je možné v editaci vypnout notifikace do slacku v určitý čas

--- app/Project/Project.php
+++ app/Project/Project.php
@@ -7,6 +7,8 @@ namespace Pd\Monitoring\Project;
  * @property string $name
  * @property string $url
  * @property \DateTime|NULL $maintenance
+ * @property string|NULL $pausedFrom
+ * @property string|NULL $pausedTo
  * @property \Nextras\Orm\Relationships\OneHasMany|\Pd\Monitoring\Check\Check[] $checks {1:m \Pd\Monitoring\Check\Check::$project}
  */
 class Project extends \Nextras\Orm\Entity\Entity
