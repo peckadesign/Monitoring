26747279a192a18ca945a262ffc22ae24bd6ac2b
Milan Pála <milan.pala@peckadesign.cz>
2017-12-21 13:43:23 +0100

    Odebrán yarn ve prospěch novějšího npm

--- vagrant/server/install.sh
+++ vagrant/server/install.sh
@@ -3,4 +3,5 @@
 touch ~/.ssh/known_hosts && ssh-keyscan -H "github.com" > ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
 grep -q "cd /vagrant" ~/.profile 2> "/dev/null" || echo "cd /vagrant" >> ~/.profile
 cd "/vagrant"
-#composer install --no-interaction
+composer install --no-interaction
+npm install
