021a9d592a5bdc65bbfc5d5d11b77cad57e35255
Milan Pála <milan.pala@peckadesign.cz>
2017-12-22 10:31:31 +0100

    Po refreshi zůstává vybraný tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,17 @@ $(function () {
 		return window.confirm('Opravdu?');
 	});
 
+	var anchor = document.location.hash;
+	if (anchor) {
+		$('.nav-tabs a[href="#' + anchor.split('#')[1] + '"]').tab('show');
+	}
+
+	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+		if (history.replaceState) {
+			history.replaceState(null, null, e.target.href);
+		} else {
+			document.location.hash = e.target.href.substring(e.target.href.indexOf("#") + 1);
+		}
+	})
+
 });
--- www/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-	$.nette.init();
-
-	$('a[data-confirm]').on('click', function() {
-		return window.confirm('Opravdu?');
-	});
-
-});
