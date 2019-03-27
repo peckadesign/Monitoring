6ae5c5f1b684992f6cfc5a8944c5b263ec28bbe1
Milan Pála <milan.pala@peckadesign.cz>
2019-03-27 13:03:04 +0100

    Instlace elastic programů vyžaduje potvrzení

--- vagrant/server/elastic.sh
+++ vagrant/server/elastic.sh
@@ -2,11 +2,11 @@
 
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 
-apt-get install apt-transport-https
+yes | apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" > /etc/apt/sources.list.d/elastic-6.x.list
 
 apt-get update
-apt-get install openjdk-11-jre-headless
-apt-get install elasticsearch
-apt-get install kibana
+yes | apt-get install openjdk-11-jre-headless
+yes | apt-get install elasticsearch
+yes | apt-get install kibana
 
