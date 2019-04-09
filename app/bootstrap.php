379a2e8be13772ac27a7873186cb0bad64ccdfe6
Milan Pála <milan.pala@peckadesign.cz>
2019-04-09 08:49:55 +0200

    Indexy musí být po dnech, jinak dochází k duplikaci hodnot

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -21,7 +21,7 @@ $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
 $configurator->addDynamicParameters([
-	'monologIndex' => 'monolog_' . \date('Y_m'),
+	'monologIndex' => 'monolog_' . \date('Y_m_d'),
 ]);
 
 $container = $configurator->createContainer();
