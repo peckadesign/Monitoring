919d537c7c98e51b1be11cf70ab442b250f199fc
Milan Pála <milan.pala@peckadesign.cz>
2021-04-30 14:09:24 +0200

    #147 Je vyžadováno aspoň jedno oprávnění

--- app/DashBoard/Controls/UserOnProject/Factory.php
+++ app/DashBoard/Controls/UserOnProject/Factory.php
@@ -32,44 +32,27 @@ final class Factory
 	{
 		$dataGrid = $this->dataGridFactory->create();
 
-		$dataGrid->setPrimaryKey('user');
-
-		$dataSource = new \Ublaboo\DataGrid\DataSource\NextrasDataSource($this->userOnProjectRepository->findBy(['project' => $project]), 'user');
-
-		$dataGrid->setDataSource($dataSource);
-
-		$cb = static function (\Pd\Monitoring\UserOnProject\UserOnProject $userOnProject): string
+		$createDataSourceCb = function () use ($project): \Ublaboo\DataGrid\DataSource\ArrayDataSource
 		{
-			return $userOnProject->user->gitHubName;
-		};
-		$dataGrid
-			->addColumnText('user', 'Uživatel')
-			->setRenderer($cb)
-		;
+			$data = [];
+			foreach ($this->userOnProjectRepository->findBy(['project' => $project]) as $userOnProject) {
+				$data[$userOnProject->user->id] = ['id' => $userOnProject->user->id, 'user' => $userOnProject->user->gitHubName, 'view' => $userOnProject->view, 'edit' => $userOnProject->edit, 'admin' => $userOnProject->admin];
+			}
 
-		$callback = static function (\Pd\Monitoring\UserOnProject\UserOnProject $userOnProject): bool
-		{
-			return $userOnProject->view;
+			return new \Ublaboo\DataGrid\DataSource\ArrayDataSource($data);
 		};
-		self::addBooleanColumn($dataGrid, 'view', 'Prohlížet', $callback);
 
-		$callback = static function (\Pd\Monitoring\UserOnProject\UserOnProject $userOnProject): bool
-		{
-			return $userOnProject->edit;
-		};
-		self::addBooleanColumn($dataGrid, 'edit', 'Upravovat', $callback);
+		$dataGrid->setDataSource($createDataSourceCb());
 
-		$callback = static function (\Pd\Monitoring\UserOnProject\UserOnProject $userOnProject): bool
-		{
-			return $userOnProject->admin;
-		};
-		self::addBooleanColumn($dataGrid, 'admin', 'Mazat', $callback);
+		$dataGrid->addColumnText('user', 'Uživatel');
 
-		$cb = static function (\Pd\Monitoring\UserOnProject\UserOnProject $userOnProject): int
-		{
-			return $userOnProject->user->id;
-		};
-		$alreadyUsers = \array_map($cb, $dataSource->getData());
+		self::addBooleanColumn($dataGrid, 'view', 'Prohlížet');
+
+		self::addBooleanColumn($dataGrid, 'edit', 'Upravovat');
+
+		self::addBooleanColumn($dataGrid, 'admin', 'Mazat');
+
+		$alreadyUsers = \array_keys($dataGrid->getDataSource()->getData());
 
 		$inlineAdd = $dataGrid->addInlineAdd();
 		$addCallback = function (\Nette\Forms\Container $container) use ($alreadyUsers): void
@@ -85,8 +68,12 @@ final class Factory
 		};
 		$inlineAdd->setPositionTop()->onControlAdd[] = $addCallback;
 
-		$inlineAdd->onSubmit[] = function ($values) use ($project): void
+		$inlineAdd->onSubmit[] = function ($values) use ($project, $createDataSourceCb, $dataGrid): void
 		{
+			if ( ! $values->view && ! $values->edit && ! $values->admin) {
+				$values->view = TRUE;
+			}
+
 			$userOnProject = new \Pd\Monitoring\UserOnProject\UserOnProject();
 			$userOnProject->user = $this->usersRepository->getById($values->user);
 			$userOnProject->project = $project;
@@ -94,6 +81,8 @@ final class Factory
 			$userOnProject->edit = $values->edit;
 			$userOnProject->admin = $values->admin;
 			$this->userOnProjectRepository->persistAndFlush($userOnProject);
+
+			$dataGrid->setDataSource($createDataSourceCb());
 		};
 
 		$inlineEdit = $dataGrid->addInlineEdit();
@@ -105,37 +94,49 @@ final class Factory
 		};
 		$inlineEdit->onControlAdd[] = $editCallback;
 
-		$inlineEdit->onSetDefaults[] = static function (\Nette\Forms\Container $container, \Pd\Monitoring\UserOnProject\UserOnProject $userOnProject): void {
-			$container->setDefaults(['view' => $userOnProject->view, 'edit' => $userOnProject->edit, 'admin' => $userOnProject->admin]);
+		$inlineEdit->onSetDefaults[] = static function (\Nette\Forms\Container $container, array $userOnProject): void
+		{
+			$container->setDefaults($userOnProject);
 		};
 
-		$inlineEdit->onSubmit[] = function ($user, $values) use ($project): void
+		$inlineEdit->onSubmit[] = function ($user, $values) use ($project, $createDataSourceCb, $dataGrid): void
 		{
+			if ( ! $values->view && ! $values->edit && ! $values->admin) {
+				$values->view = TRUE;
+			}
+
 			$userOnProject = $this->userOnProjectRepository->getBy(['user' => $user, 'project' => $project]);
 			$userOnProject->view = $values->view;
 			$userOnProject->edit = $values->edit;
 			$userOnProject->admin = $values->admin;
 			$this->userOnProjectRepository->persistAndFlush($userOnProject);
+
+			$dataGrid->setDataSource($createDataSourceCb());
 		};
+
+		$cb = function (string $id) use ($project, $createDataSourceCb, $dataGrid): void
+		{
+			$userOnProject = $this->userOnProjectRepository->getBy(['user' => $id, 'project' => $project]);
+			$this->userOnProjectRepository->removeAndFlush($userOnProject);
+
+			$dataGrid->setDataSource($createDataSourceCb());
+		};
+		$confirmation = new \Ublaboo\DataGrid\Column\Action\Confirmation\StringConfirmation('Opravdu?');
+		$dataGrid->addActionCallback('delete', 'Odebrat', $cb)->setConfirmation($confirmation);
 
 		return $dataGrid;
 	}
 
 
-	private static function addBooleanColumn(\Ublaboo\DataGrid\DataGrid $dataGrid, string $key, string $name, callable $callback): \Ublaboo\DataGrid\Column\Column
+	private static function addBooleanColumn(\Ublaboo\DataGrid\DataGrid $dataGrid, string $key, string $name): \Ublaboo\DataGrid\Column\Column
 	{
-		$cb = static function (\Pd\Monitoring\UserOnProject\UserOnProject $userOnProject) use ($callback): string
-		{
-			return $callback($userOnProject) ? 'Ano' : 'Ne';
-		};
-
 		$column = $dataGrid
 			->addColumnText($key, $name)
 			->setFitContent(TRUE)
-			->setRenderer($cb)
+			->setReplacement([TRUE => 'Ano', FALSE => 'Ne'])
 		;
 
-		$dataGrid->addColumnCallback($column->getColumnName(), static function (\Ublaboo\DataGrid\Column\Column $column, \Pd\Monitoring\UserOnProject\UserOnProject $userOnProject) use ($callback): void
+		$dataGrid->addColumnCallback($column->getColumnName(), static function (\Ublaboo\DataGrid\Column\Column $column, array $userOnProject) use ($key): void
 		{
 			$td = $column->getElementPrototype('td');
 			$classes = (array) $td->getAttribute('class');
@@ -143,7 +144,7 @@ final class Factory
 			unset($classes[self::CLASS_GREEN]);
 			unset($classes[self::CLASS_RED]);
 
-			$class = $callback($userOnProject) ? self::CLASS_GREEN : self::CLASS_RED;
+			$class = $userOnProject[$key] ? self::CLASS_GREEN : self::CLASS_RED;
 
 			$classes[$class] = TRUE;
 			$td->setAttribute('class', $classes);
