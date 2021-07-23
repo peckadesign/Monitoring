7926d39aec73d15aca0ce16a4222e25152cafbde
Milan Pála <milan.pala@peckadesign.cz>
2021-07-23 12:24:29 +0200

    Databáze vzniká s admin uživatelem

---
+++ migrations/basic-data/2021-07-23-102730-init.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (`git_hub_id`, `git_hub_name`, `git_hub_token`, `system_user`, `administrator`, `slack_id`, `password`, `authtoken`, `email`)
+VALUES ('123456', 'Admin', '', '0', '1', NULL, '$2y$10$R6Af96TXjiyBiwqAqHjvS.GF/X1IRTf/3PKl5l0Vva9tecQWwg17y', NULL, 'admin@localhost.local');
