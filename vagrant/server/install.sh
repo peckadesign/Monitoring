906b81279fc358c22143734d2b2c5cb939f28bef
jakubenglicky <jakubenglicky@gmail.com>
2021-09-04 11:26:41 +0200

    Odebrání Vagrantu
    - nyní na něm nevyvíjíme a není nutné ho tedy udržovat

--- vagrant/server/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-touch ~/.ssh/known_hosts && ssh-keyscan -H "github.com" > ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
-grep -q "cd /vagrant" ~/.profile 2> "/dev/null" || echo "cd /vagrant" >> ~/.profile
-cd "/vagrant"
-composer install --no-interaction
-npm install
--- vagrant/server/load.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-service apache2 restart
-service rabbitmq-server restart
-service elasticsearch restart
-service kibana restart
-service mysql restart
