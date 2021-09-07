6ab2fffcd40a30e44e3813671250fa4972719e55
Milan Pála <milan.pala@peckadesign.cz>
2021-09-07 12:49:12 +0200

    Správné pořadí parametrů

--- monitoring-start.sh
+++ monitoring-start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-docker-compose -f docker-compose.full.yml up database rabbitmq elasticsearch -d
+docker-compose -f docker-compose.full.yml up -d database rabbitmq elasticsearch
 docker-compose -f docker-compose.full.yml exec database bash -c "until mysqladmin ping -hdatabase -umonitoring -pmonitoring; do sleep 2; done"
 docker-compose -f docker-compose.full.yml exec elasticsearch bash -c "until curl -s http://elasticsearch:9200/; do sleep 2; done"
 docker-compose -f docker-compose.full.yml exec rabbitmq bash -c "until rabbitmqctl ping; do sleep 2; done"
