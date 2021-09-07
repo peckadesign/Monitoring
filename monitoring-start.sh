fc93038bccf1c628fa7d50156c45e1784c8681ce
Milan Pála <milan.pala@peckadesign.cz>
2021-09-07 12:10:17 +0200

    Docker compose není všude

--- monitoring-start.sh
+++ monitoring-start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-docker compose -f docker-compose.full.yml up database rabbitmq elasticsearch -d
-docker compose -f docker-compose.full.yml exec database bash -c "until mysqladmin ping -hdatabase -umonitoring -pmonitoring; do sleep 2; done"
-docker compose -f docker-compose.full.yml exec elasticsearch bash -c "until curl -s http://elasticsearch:9200/; do sleep 2; done"
-docker compose -f docker-compose.full.yml exec rabbitmq bash -c "until rabbitmqctl ping; do sleep 2; done"
-docker compose -f docker-compose.full.yml run web bin/console migrations:continue
-docker compose -f docker-compose.full.yml run web bin/console rabbitmq:setup-fabric
-docker compose -f docker-compose.full.yml up -d
+docker-compose -f docker-compose.full.yml up database rabbitmq elasticsearch -d
+docker-compose -f docker-compose.full.yml exec database bash -c "until mysqladmin ping -hdatabase -umonitoring -pmonitoring; do sleep 2; done"
+docker-compose -f docker-compose.full.yml exec elasticsearch bash -c "until curl -s http://elasticsearch:9200/; do sleep 2; done"
+docker-compose -f docker-compose.full.yml exec rabbitmq bash -c "until rabbitmqctl ping; do sleep 2; done"
+docker-compose -f docker-compose.full.yml run web bin/console migrations:continue
+docker-compose -f docker-compose.full.yml run web bin/console rabbitmq:setup-fabric
+docker-compose -f docker-compose.full.yml up -d
